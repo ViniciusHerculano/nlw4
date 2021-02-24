4e5065b51a6b29ba943bbdcd299955c6ce00c949
Vinicius Herculano <viniciusherculanomap@gmail.com>
2021-02-24 10:22:25 -0300

    aula 02 finalizada

--- aulas/api/src/server.ts
+++ aulas/api/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { response } from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
@@ -14,16 +15,10 @@ const app = express();
 
 //http://localhost:3333/users
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World!" });
-})
-
 // 1 param => Rota(Recurso API)
 //2 param => Request, response
 
-app.post("/", (request, response) => {
-    //Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso!" });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is runing!"));
